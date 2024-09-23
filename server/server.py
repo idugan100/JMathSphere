9d1b73b9d88badbb9042743ff42253470a582e32
idugan100 <isaacd4444@gmail.com>
2024-09-23 15:29:38 -0400

    added fastapi server

---
+++ server/server.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.post("/hi")
+def color_form(favcolor:Annotated[str, Form()], color: Annotated[str, Form()], country: Annotated[str, Form()],Gender: Annotated[str, Form()]):
+    # save to a csv file
+    # host server on aws so it can be accessed from the internet
+    # get james setup with ssh credentials into server so he can see the data
+    print(favcolor, color, country, Gender)
+    html_content = """
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>JMathSphere</title>
+            <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://unpkg.com/chimeracss/build/chimera-dark.css"
+        />
+        </head>
+        <body>
+            <h1>hi</h1>
+            <p>thanks for your response!</p>
+            <button style="background-color: aliceblue;">
+                <a href="https://idugan100.github.io/JMathSphere/">Home</a>
+            
+            </button>
+        </body>
+        </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
